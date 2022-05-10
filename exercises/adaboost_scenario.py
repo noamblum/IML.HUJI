7aca8c020a2ca219b255cd5b85ef7de2002d5cac
Noam Blum <noamblum756@gmail.com>
2022-05-10 12:25:35 +0300

    Finished Ex4 question 2

--- exercises/adaboost_scenario.py
+++ exercises/adaboost_scenario.py
@@ -41,6 +41,7 @@ def generate_data(n: int, noise_ratio: float) -> Tuple[np.ndarray, np.ndarray]:
 def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=500):
     (train_X, train_y), (test_X, test_y) = generate_data(train_size, noise), generate_data(test_size, noise)
 
+    s = train_y[train_y == 1]
     # Question 1: Train- and test errors of AdaBoost in noiseless case
     def wl():
         return DecisionStump()
@@ -64,7 +65,21 @@ def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=
     # Question 2: Plotting decision surfaces
     T = [5, 50, 100, 250]
     lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
-    raise NotImplementedError()
+    fig = make_subplots(rows=2, cols=2, subplot_titles=[f"{t} Weak Learners" for t in T],
+                    horizontal_spacing = 0.01, vertical_spacing=.03)
+    symbols = np.array(["x", "circle", "x"])
+    for i, t in enumerate(T):
+        p = lambda X: ab.partial_predict(X,t)
+        fig.add_traces([decision_surface(p, lims[0], lims[1], showscale=False),
+                    go.Scatter(x=test_X[:,0], y=test_X[:,1], mode="markers", showlegend=False,
+                               marker=dict(color=test_y.astype(int), symbol=symbols[test_y.astype(int)], colorscale=[custom[0], custom[-1]], 
+                                           line=dict(color="black", width=1)) )], 
+                   rows=(i//2) + 1, cols=(i%2)+1)
+
+    fig.update_layout(title="Decision boundaries for different amounts of weak learners", margin=dict(t=100))\
+    .update_xaxes(visible=False). update_yaxes(visible=False)
+
+    fig.show()
 
     # Question 3: Decision surface of best performing ensemble
     raise NotImplementedError()
